2977fb8dd2f568de8e48ee6ac1a26d33de341d72
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-11-20 16:43:08 -0500

    chore: update packages

--- src/OpenMatchFrontend/Program.cs
+++ src/OpenMatchFrontend/Program.cs
@@ -52,6 +52,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Graceful Termination for Kubernetes
 CancellationTokenSource cancellation = new();
 app.Lifetime.ApplicationStopping.Register(() =>
 {
