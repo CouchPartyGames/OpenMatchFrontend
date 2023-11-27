8843720191a4a77ca137e0a8fe558e295c535444
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-11-27 10:51:46 -0500

    feat: add global error handler

---
+++ src/OpenMatchFrontend/Exceptions/DefaultExceptionHandler.cs
@@ -0,0 +1,24 @@
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenMatchFrontend.Exceptions;
+
+public class DefaultExceptionHandler(ILogger<DefaultExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError(exception, "An unexpected error occurred");
+
+        await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+        {
+            Status = (int)HttpStatusCode.InternalServerError,
+            Type = exception.GetType().Name,
+            Title = "An unexpected error occurred",
+            Detail = exception.Message,
+            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}"
+        });
+
+        return true;
+    }
+}
