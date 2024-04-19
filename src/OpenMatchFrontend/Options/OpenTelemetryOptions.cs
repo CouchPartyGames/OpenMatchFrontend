b703bcbe4041a0bc6c5e4167df2df0df78ace543
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-04-19 16:27:30 -0400

    feat: add configure options for opentelemetry

--- src/OpenMatchFrontend/Options/OpenTelemetryOptions.cs
+++ src/OpenMatchFrontend/Options/OpenTelemetryOptions.cs
@@ -5,4 +5,8 @@ public sealed class OpenTelemetryOptions
    public const string SectionName = "OpenTelemetry";
 
    public string Endpoint { get; init; } = "http://localhost:4317";
+
+   public string Protocol { get; init; } = "grpc";
+
+   public float SampleRate { get; init; } = 1.0f;
 }
